aea2328fe8c7ad0c858a4a960fce4435402b4628
Robin9800 <robin.costanzo@gmail.com>
2022-05-05 14:21:29 +0200

    generics fixing

--- index.php
+++ index.php
@@ -114,9 +114,6 @@
              foreach ($faq as $key => $item) {
                 echo('<h2>' . $item['domanda'] . '</h2>');
                 $itemRisposta = explode('|||', $item['risposta']);
-                // echo('<pre>');
-                // var_dump($itemRisposta);
-                // echo('</pre>');
                 foreach($itemRisposta as $paragrafo){
                     echo('<p>' . $paragrafo . '</p>');
                 }
